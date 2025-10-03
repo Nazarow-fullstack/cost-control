109d2faa3314929005999db3b72133544f806f21
hia-03 <habibulozoda1963@gmail.com>
2025-10-03 20:38:01 +0500

    profilesignals

--- authentication/models.py
+++ authentication/models.py
@@ -6,3 +6,6 @@ class CustomUser(AbstractUser):
         ('admin', 'Admin')
     )
     role = models.CharField(max_length=10, choices=CHOISES, default='user')
+
+    def __str__(self):
+        return self.username
--- profile/models.py
+++ profile/models.py
@@ -4,3 +4,6 @@ class Profile(models.Model):
     user_id = models.OneToOneField("authentication.CustomUser",  on_delete=models.CASCADE)
     name = models.CharField( max_length=50)
     bio = models.TextField()
+
+    def __str__(self):
+        return self.name
